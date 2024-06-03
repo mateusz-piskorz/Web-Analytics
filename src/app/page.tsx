b1a7f6cc755e0e40082e648e20d6db15542fbb79
Epicka Owca <epickaowca@Epickas-Laptop.local>
2024-06-03 14:56:28 +0200

    main page navigation

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,30 @@
+import styles from "./page.module.scss";
+import Link from "next/link";
+
+const navList = [
+  { name: "Audiophile", href: "audiophile" },
+  { name: "Google Drive Clone", href: "google-drive-clone" },
+  {
+    name: "Interactive Comments Section",
+    href: "interactive-comments-section",
+  },
+  { name: "Multi Step Form", href: "google-drive-clone" },
+];
+console.log(styles);
 export default function Home() {
   return (
     <main>
+      <nav>
+        <ul className={styles.List}>
+          {navList.map((item, index) => (
+            <li key={index}>
+              <Link className={styles.List_Item} href={item.href}>
+                {item.name}
+              </Link>
+            </li>
+          ))}
+        </ul>
+      </nav>
       <h1>Main Page</h1>
     </main>
   );
