3d7b597db230f6b142c5498bea6346afbc49a422
Epicka Owca <epickaowca@Epickas-Laptop.local>
2024-06-03 12:58:22 +0200

    ActivityGraph init

---
+++ src/app/[projectId]/(features)/ActivityGraph/components/Chart.tsx
@@ -0,0 +1,22 @@
+import React, { FC } from "react";
+
+const visitorsData = [
+  { x: new Date("2007-04-24"), y: 0 },
+  { x: new Date("2007-04-25"), y: 0 },
+  { x: new Date("2007-04-26"), y: 0 },
+  { x: new Date("2007-04-27"), y: 0 },
+  { x: new Date("2007-04-28"), y: 2 },
+  { x: new Date("2007-04-29"), y: 5 },
+  { x: new Date("2007-04-30"), y: 2 },
+];
+
+export const Chart: FC<{}> = () => {
+  return (
+    <div>
+      <svg width="100%">
+        <rect x={0} y={0} width="100%" />
+      </svg>
+    </div>
+  );
+};
+//
--- src/app/[projectId]/page.tsx
+++ src/app/[projectId]/page.tsx
@@ -1,9 +1,10 @@
 import React, { FC } from "react";
+import { ActivityGraph } from "./(features)/ActivityGraph";
 
 const Page: FC<{ params: { projectId: string } }> = ({
   params: { projectId },
 }) => {
-  return <div>{projectId}</div>;
+  return <ActivityGraph />;
 };
 
 export default Page;
