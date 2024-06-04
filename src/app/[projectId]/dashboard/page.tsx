21454b7f20d83e2a15068609311f01209fe6f8da
Epicka Owca <epickaowca@Epickas-Laptop.local>
2024-06-04 22:36:14 +0200

    add List Container

--- src/app/[projectId]/dashboard/page.tsx
+++ src/app/[projectId]/dashboard/page.tsx
@@ -1,6 +1,7 @@
 import React, { FC } from "react";
 import { ActivityGraph } from "./(features)/ActivityGraph";
 import { ActivityList } from "./(features)/ActivityList";
+import style from "./styles.module.scss";
 
 type DashboardProps = { params: { projectId: string } };
 
@@ -9,11 +10,27 @@ const countriesArr = [
   { name: "Belgium", quantity: 4 },
 ];
 
+const browsersArr = [
+  { name: "Chrome", quantity: 17 },
+  { name: "Safari", quantity: 4 },
+  { name: "Opera", quantity: 2 },
+];
+
+const OSArr = [
+  { name: "Linux", quantity: 69 },
+  { name: "Mac", quantity: 27 },
+  { name: "Windows", quantity: 4 },
+];
+
 const Dashboard: FC<DashboardProps> = ({ params: { projectId } }) => {
   return (
     <>
       <ActivityGraph />
-      <ActivityList title="Countries" list={countriesArr} />
+      <div className={style.ActivityListContainer}>
+        <ActivityList title="Countries" list={countriesArr} />
+        <ActivityList title="Browsers" list={browsersArr} />
+        <ActivityList title="Operating Systems" list={OSArr} />
+      </div>
     </>
   );
 };
