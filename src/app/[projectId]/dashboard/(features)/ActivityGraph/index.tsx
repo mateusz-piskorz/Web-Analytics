af43d8c1ffed597f41fcdbf27d724513a1c531c2
Epicka Owca <epickaowca@Epickas-Laptop.local>
2024-06-04 10:51:49 +0200

    update layout styles

--- src/app/[projectId]/dashboard/(features)/ActivityGraph/index.tsx
+++ src/app/[projectId]/dashboard/(features)/ActivityGraph/index.tsx
@@ -6,7 +6,13 @@ import styles from "./styles.module.scss";
 export const ActivityGraph = () => {
   return (
     <div className={styles.Wrapper}>
-      <div className={styles.Wrapper_Header}></div>
+      <div className={styles.Wrapper_Header}>
+        <div className={styles.Wrapper_Visitors}>
+          <span className={styles.Wrapper_VisitorsText}>Visitors</span>
+          <span className={styles.Wrapper_VisitorsNumber}>42</span>
+        </div>
+        <span className={styles.Wrapper_Percentages}>+3.94%</span>
+      </div>
       <Chart />
     </div>
   );
