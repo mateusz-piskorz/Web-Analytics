ebe8c52f88d03eee70de9783a92898e2671d91a7
Epicka Owca <epickaowca@Epickas-Laptop.local>
2024-06-06 17:23:53 +0200

    cors

--- src/app/[projectName]/api/route.ts
+++ src/app/[projectName]/api/route.ts
@@ -1,12 +1,19 @@
-import { NextResponse, NextRequest } from "next/server";
+import { NextRequest } from "next/server";
 import { PrismaClient } from "@prisma/client";
 
 const db = new PrismaClient();
+
+const allowedOrigins = process.env.CORS?.split(",");
 
 export async function POST(
   request: NextRequest,
   { params: { projectName } }: { params: { projectName: string } }
 ) {
+  const origin = request.headers.get("origin");
+  if (!origin || !allowedOrigins?.includes(origin)) {
+    return sendResponse(400, { message: "origin not allowed" });
+  }
+
   const {
     country = "unknown",
     OS = "unknown",
@@ -17,23 +24,24 @@ export async function POST(
     const project = await db.project.findUnique({
       where: { name: projectName },
     });
-    if (!project) return throwError(404);
+    if (!project) return sendResponse(404, { message: "project not found" });
 
     const analytic = await db.analytic.create({
       data: { browser, country, OS, projectId: project.id },
     });
 
-    return NextResponse.json(analytic);
+    return sendResponse(200, { analytic });
   } catch (error: any) {
-    return NextResponse.json({ hello: error.message });
+    return sendResponse(400, { message: error.message });
   }
 }
 
-const throwError = (status: 404) => {
-  return NextResponse.json(
-    {
-      message: "No project found for the given project name",
+const sendResponse = (status: number, message: any) => {
+  return new Response(JSON.stringify(message), {
+    status: status,
+    headers: {
+      "Access-Control-Allow-Origin": "*",
+      "Access-Control-Allow-Methods": "GET, POST, PUT, DELETE, OPTIONS",
     },
-    { status: status }
-  );
+  });
 };
