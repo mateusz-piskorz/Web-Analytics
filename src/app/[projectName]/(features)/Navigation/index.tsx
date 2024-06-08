e18515bbfa8b21e613458db9a6278251eb9244a6
Epicka Owca <epickaowca@Epickas-Laptop.local>
2024-06-08 13:03:20 +0200

    style navigation depending on path

--- src/app/[projectName]/(features)/Navigation/index.tsx
+++ src/app/[projectName]/(features)/Navigation/index.tsx
@@ -1,8 +1,11 @@
+"use client";
 import React, { FC } from "react";
 import styles from "./Navigation.module.scss";
 import Link from "next/link";
 import { AnalyticsSvg } from "./components/AnalyticsSvg";
 import { DashboardSvg } from "./components/DashboardSvg";
+
+import { usePathname } from "next/navigation";
 
 type NavigationProps = {
   projectName: string;
@@ -11,36 +14,39 @@ type NavigationProps = {
   desktopCase?: boolean;
 };
 
+const linkArr = [
+  { href: "dashboard", label: "Dashboard" },
+  { href: "analytics", label: "Analytics" },
+];
+
 export const Navigation: FC<NavigationProps> = ({
   projectName,
   isNavOpen,
   setNavClose,
 }) => {
+  const pathname = usePathname();
+  const arr = pathname.split("/");
+  const currentPath = arr[arr.length - 1];
+
   return (
     <nav
       className={`${styles.Nav}${!isNavOpen ? " " + styles.Nav__hidden : ""}`}
     >
       <ul className={styles.Nav_List}>
-        <li className={styles.Nav_Item}>
-          <Link
-            onClick={setNavClose}
-            className={`${styles.Nav_Link} ${styles.Nav_Link__active}`}
-            href={`/${projectName}/dashboard?analyticPeriod=7`}
-          >
-            <DashboardSvg />
-            <span>Dashboard</span>
-          </Link>
-        </li>
-        <li className={styles.Nav_Item}>
-          <Link
-            onClick={setNavClose}
-            className={styles.Nav_Link}
-            href={`/${projectName}/analytics?analyticPeriod=7`}
-          >
-            <AnalyticsSvg />
-            <span>Analytics</span>
-          </Link>
-        </li>
+        {linkArr.map(({ href, label }) => (
+          <li className={styles.Nav_Item} key={href}>
+            <Link
+              onClick={setNavClose}
+              className={`${styles.Nav_Link}${
+                currentPath === href ? " " + styles.Nav_Link__active : ""
+              }`}
+              href={`/${projectName}/${href}?analyticPeriod=7`}
+            >
+              <DashboardSvg />
+              <span>{label}</span>
+            </Link>
+          </li>
+        ))}
       </ul>
     </nav>
   );
