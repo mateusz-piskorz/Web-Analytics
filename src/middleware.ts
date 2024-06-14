6f0f7724a6e894363460a9e4190f6aa012a7ee7a
Epicka Owca <epickaowca@Epickas-Laptop.local>
2024-06-14 07:51:41 +0200

    add same origin cors logic

--- src/middleware.ts
+++ src/middleware.ts
@@ -5,12 +5,17 @@ const allowedOrigins = process.env.CORS?.split(",");
 export function middleware(request: NextRequest) {
   const response = NextResponse.next();
   const origin = request.headers.get("origin");
+  const isSameOrigin = request.headers.get("Sec-Fetch-Site") === "same-origin";
 
   const isCorsDisabled = allowedOrigins === undefined;
   const isCorsAllowAll = allowedOrigins?.includes("*");
   const isOriginAllowed = origin && allowedOrigins?.includes(origin);
 
-  if (!isCorsAllowAll && (isCorsDisabled || !isOriginAllowed)) {
+  if (
+    !isSameOrigin &&
+    !isCorsAllowAll &&
+    (isCorsDisabled || !isOriginAllowed)
+  ) {
     return new Response(JSON.stringify(`${origin} - origin not allowed`), {
       status: 500,
     });
